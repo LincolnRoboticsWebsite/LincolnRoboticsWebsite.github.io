08230325cc021914bb8c3fa6cca4cab14c3f1333
LincolnRoboticsWebsite <38081393+LincolnRoboticsWebsite@users.noreply.github.com>
2018-04-25 10:11:13 -0700

    Add files via upload

---
+++ scripts/onClickDiv.js
@@ -0,0 +1,15 @@
+function onClickHome(){
+	window.location.href = 'index.html';
+}
+function onClickTeams(){
+	window.location.href = 'teams.html';
+}
+function onClickMenotrs(){
+	window.location.href = 'mentors.html';
+}
+function onClickDonate(){
+	window.location.href = 'donate.html';
+}
+function onClickAbout(){
+	window.location.href = 'about.html';
+}
---
+++ scripts/slideshow.js
@@ -0,0 +1,48 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+carousel();
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function dispSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("slide_image");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+}
+
+function carousel() {
+	var i;
+	var slides = document.getElementsByClassName("slide_image");
+	var dots = document.getElementsByClassName("dot");
+	if (slideIndex > slides.length) {slideIndex = 1}
+	
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	
+	slideIndex++;
+	
+	showSlides(slideIndex + 1);
+	setTimeout(carousel, 5000);
+}
