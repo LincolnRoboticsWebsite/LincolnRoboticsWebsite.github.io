e1e6cf4d9369eabf87d42cab9a844398b13f12a7
LincolnRoboticsWebsite <zhaojonathan99@gmail.com>
2018-08-28 20:16:05 -0700

    added a test version of draggable box, although javascript will need to detect x position in relation to window size and then redirect to the parenttest.html, and we should have a large box that follows to cover up the website, and it should automatically slide over once it reaches a certain point

---
+++ scripts/thezone.js
@@ -0,0 +1,41 @@
+//Make the DIV element draggagle:
+dragElement(document.getElementById(("zone")));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos3 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos3 = e.clientX;
+    if (elmnt.offsetLeft - pos1 > window.innerWidth - 17) {
+      elmnt.style.left = (window.innerWidth - 17) + "px";
+    } else{
+      // set the element's new position:
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
