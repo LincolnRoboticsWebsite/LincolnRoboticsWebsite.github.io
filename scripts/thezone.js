ee55dcb43e881edae19ad5d88c5cc8d2c7523f7a
LincolnRoboticsWebsite <wmccormick4732.awesome@gmail.com>
2018-09-05 08:49:17 -0700

    added more to the parent zone

--- scripts/thezone.js
+++ scripts/thezone.js
@@ -3,18 +3,20 @@ dragElement(document.getElementById(("zone")));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos3 = 0;
-//  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-//    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-//  }
+  elmnt.onmousedown = dragMouseDown;
+
+  function offset() {
+    if (document.getElementById(elmnt.id + "follow")) {
+      /* if present, the follow connects to the DIV:*/
+      document.getElementById(elmnt.id + "follow").style.left = elmnt.offsetLeft + "px";
+    }
+  }
 
   function dragMouseDown(e) {
     e = e || window.event;
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
+    offset();
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
@@ -25,34 +27,43 @@ function dragElement(elmnt) {
     // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos3 = e.clientX;
-//    if (elmnt.offsetLeft - pos1 > window.innerWidth - 17) {
-//      elmnt.style.left = (window.innerWidth - 17) + "px";
-//    } else {
-      // set the element's new position:
-      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-//    }
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    offset();
   }
 
   function closeDragElement() {
     /* stop moving when mouse button is released:*/
     document.onmouseup = null;
     document.onmousemove = null;
-
-    /*if(elmnt.offsetLeft < window.innerWidth * .25){
-      elmnt.style.left = "0px";
-    }*/
+    offset();
     if(elmnt.offsetLeft < .75 * window.innerWidth && elmnt.offsetLeft > 0){
       var q = setInterval(animate, 3);
-      var pos = elmnt.offsetLeft;
+    } else{
+      var q = setInterval(unanimate, 3);
     }
 
     function animate(){
+      var pos = elmnt.offsetLeft;
       if(elmnt.offsetLeft <= 0){
         clearInterval(q);
+        window.location = "file:///C:/Users/zhaoj/Documents/GitHub/LincolnRoboticsWebsite.github.io/parenttest.html";
       }
       else {
         pos -= 5;
         elmnt.style.left = pos + "px";
+        offset();
+      }
+    }
+
+    function unanimate(){
+      var pos = elmnt.offsetLeft;
+      if(elmnt.offsetLeft >= window.innerWidth - elmnt.clientWidth / 6){
+        clearInterval(q);
+      }
+      else {
+        pos += 2;
+        elmnt.style.left = pos + "px";
+        offset();
       }
     }
   }
